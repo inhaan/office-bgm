8b95ec4f31ab08501c3ffe5ea9670583ef62c288
강인한 <hunt2433@naver.com>
2024-07-06 17:00:19 +0900

    feat: add contact us link to footer

--- app/page.tsx
+++ app/page.tsx
@@ -92,6 +92,9 @@ export default function Home() {
                             >
                                 <FaGithub />
                             </a>
+                            <a href="https://forms.gle/REpgswzVYHWvpeoB7" target="_blank" rel="noopener noreferrer">
+                                문의하기
+                            </a>
                         </div>
                         <div>
                             <a href="https://iconscout.com/icons/music" target="_blank">
